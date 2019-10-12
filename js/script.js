a2cb06b389f89ca6126e1926da1b32fc59cde0bf
Chad Ketcham <chadketcham@gmail.com>
2019-10-12 12:18:41 -0500

    Restyles Head to include JQery

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 console.log("Hello World")
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
+
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 7000);
+          return false;
+        }
+      }
+    });
+  });
