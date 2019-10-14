69a0e65ac946e3ad9b33fe28d88b89fae6f7d961
Chad Ketcham <chadketcham@gmail.com>
2019-10-13 21:01:53 -0500

    Menu Fixes

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 console.log("Hello World")
 
-function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-      x.className += ' responsive';
+function myNavtoggle() {
+    var x = document.getElementById("myNavtoggle");
+    if (x.className === "navtoggle") {
+      x.className += " responsive";
     } else {
-      x.className = 'navtoggle';
+      x.className = "topnav";
     }
   }
 
   $(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
+    // $('a[href*=#]:not([href=#])').click(function() { change to below
+    // ref: https://github.com/jquery/jquery/issues/2885
+    $('a[href*=\\#]:not([href=\\#])').click(function() {
       if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
